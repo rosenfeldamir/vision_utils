169a81fbd00c572cb3181830c2ea870b06e0e820
amir <amir@cse.yorku.ca>
2019-05-15 11:25:06 -0400

    simplified stuff.

--- vision_utils/simple_training.py
+++ vision_utils/simple_training.py
@@ -10,158 +10,100 @@ import glob
 from time import time
 import shutil
 from os.path import expanduser
-modelsBaseDir = os.path.expanduser('~/models')
 homeDir = expanduser('~')
-experiment_base_dir = expanduser('~/experiments/')
 
+class AverageMeter(object):
+    """Computes and stores the average and current value
+       Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
+    """
 
-def make_experiment_dir_name(experiment_params):
-    s = ''
-    for i, (k, v) in enumerate(experiment_params.items()):
-        s = s + k + '_' + str(v)
-        if i < len(experiment_params):
-            s += '_'
-    return s
+    def __init__(self):
+        self.reset()
 
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
 
-def make_experiment(experiment_name, experiment_params):
-    exp_dir_name = make_experiment_dir_name(experiment_params)
-    models_base = os.path.join(
-        experiment_base_dir, experiment_name, 'checkpoints', exp_dir_name)
-    os.makedirs(models_base, exist_ok=True)
-    logger_path = os.path.join(
-        experiment_base_dir, experiment_name, 'logs', exp_dir_name)
-    os.makedirs(logger_path, exist_ok=True)
-    return models_base, logger_path
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
 
-def matVar(size=(1, 3, 64, 64), cuda=False):
-    v = Variable(torch.randn(size))
-    if cuda:
-        v = v.cuda()
-    return v
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
 
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
-def train(model, epoch, optimizer, maxIters=np.inf, targetTranslator=None, train_loader=None, criterion=None, logger=None,
-          device='cuda'):
+def train(model, epoch, optimizer, maxIters=np.inf, train_loader=None,
+          criterion=None, device='cuda:0'):
     T0 = time()
     model.train()
-    nBatches = 0
-    running_loss = 0.0
-    running_loss2 = 0.0
-    losses = []
-    nSamples = 0
+    losses = AverageMeter()
+    top1 = AverageMeter()
     maxIters = min(maxIters, len(train_loader))
     startTime = time()
-    test_accuracy = []
-    correct = 0
     nSamples = 0
-
     for batch_idx, (data, target) in enumerate(train_loader):
-        if targetTranslator is not None:
-            target2 = targetTranslator(target.clone())
-            target2 = data.cuda(), target.cuda(), target2.cuda()
-        target = target.long().squeeze()
-        data = data.to(device)
-        target = target.to(device)
-        #, Variable(target2)
+        # target = target.long().squeeze()
+        data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        if type(output) is tuple:
-            gates = output[1]
-            output = output[0]
-
         loss = criterion(output, target)  # + criterion(output2,target2)
         # for testing accuracy on train...
-        pred = output.max(1)[1]  # get the index of the max log-probability
-        correct += pred.eq(target).cpu().sum().item()
+        prec1 = accuracy(output.data, target.data, topk=(1,))[0]
+        losses.update(loss.item())
+        top1.update(prec1.item())
         nSamples += len(data)
         loss.backward()
         optimizer.step()
-        losses.append(loss.item())
-        running_loss += loss.item()
-
-        nBatches += 1  # len(data)
         nSamples += len(data)
-        if batch_idx % 5 == 0 and time() - T0 > .2:
+        if time() - T0 > .5:  # update every half second
             T0 = time()
             elapsedTime = time() - startTime
-
-            S = 'Train Epoch: \t{epoch} [{iters}/{total} (\t{t2:.0f}%)]\tAvg Loss: \t{t3:.6f}\t({t5:.2f} imgs/sec)'.format(epoch=epoch, iters=batch_idx * len(data),
-                                                                                                                           total=len(
-                                                                                                                               train_loader.dataset),
-                                                                                                                           t2=100. * batch_idx /
-                                                                                                                           len(
-                                                                                                                               train_loader),
-                                                                                                                           t3=running_loss / nBatches,
-                                                                                                                           t5=nSamples / elapsedTime)
-
-            if logger is not None:
-                logger.log_value('training loss', loss.item(),
-                                 batch_idx + epoch * maxIters)
+            S = 'Train Epoch: \t{epoch} [{iters}/{total}] (\t{t2:.0f}%)]\tAvg Loss: \t{running_loss:.6f}\t({imgs_sec:.2f} imgs/sec)'
+            S = S.format(epoch=epoch, iters=batch_idx * len(data),
+                         total=len(train_loader.dataset), t2=100. * batch_idx / len(train_loader),
+                         running_loss=losses.avg, imgs_sec=nSamples / elapsedTime)
             print('\r{}'.format(S), end="")
         if batch_idx > maxIters:
+            print('stopping training early (debug mode')
             break
-    # b1
-    if logger is not None:
-        if hasattr(optimizer, 'param_groups'):
-
-            for param_group in optimizer.param_groups:
-                cur_lr = param_group['lr']
-                logger.log_value('learning rate', cur_lr, epoch)
-
-        if hasattr(optimizer, 'get_lr_factor'):
-            logger.log_value('learning rate', optimizer.get_lr_factor(), epoch)
-    return losses, correct / nSamples
+    return losses.avg, top1.avg
 
 
-def test(model, epoch, targetTranslator=None, test_loader=None, prev_acc=0, alpha=None, criterion=None, maxIters=np.inf, logger=None,
-         device='cuda'):
-    assert (criterion is not None)
+def test(model, epoch, test_loader=None,  criterion=None, maxIters=np.inf, device='cuda:0'):
     model.eval()
-    test_loss = 0
-    correct = 0
-    nSamples = 0
+    losses = AverageMeter()
+    top1 = AverageMeter()
     maxIters = min(maxIters, len(test_loader))
     for batch_idx, (data, target) in enumerate(tqdm(test_loader, total=len(test_loader))):
-        target = target.long().squeeze()
-        if targetTranslator is not None:
-            target2 = targetTranslator(target.clone())
-            target2 = target2.cuda()
-
         data, target = data.to(device), target.to(device)
-        if alpha is not None:
-            output = model(data, alpha)
-        else:
-            # b1
-            output = model(data)
-            if type(output) is tuple:
-                gates = output[1]
-                output = output[0]
-        cur_test_loss = criterion(output, target).item()
-        test_loss += cur_test_loss
-
-        pred = output.max(1)[1]  # get the index of the max log-probability
-        correct += pred.eq(target).cpu().sum().item()
-        nSamples += len(data)
+        output = model(data)
+        prec1 = accuracy(output.data, target.data, topk=(1,))[0]
+        loss = criterion(output, target)
+        losses.update(loss.item())
+        top1.update(prec1.item())
         if batch_idx >= maxIters:
+            print('stopping validation early (debug mode')
             break
-
-    # loss function already averages over batch size
-    test_loss /= len(test_loader)
-    if logger is not None:
-        logger.log_value('test loss', test_loss, epoch)
-    cur_acc = 100. * correct / nSamples
-    # if prev_acc < cur_acc:
-    P = '({}) :Test set: Avg. loss: \t{:.4f}, Acc: \t{}/{} ({:.1f}%)'.format(epoch,
-                                                                             test_loss, correct, nSamples, cur_acc)
-    if logger is not None:
-        logger.log_value('test accuracy', cur_acc, epoch)
-
+    P = '({epoch}) :[Test set] Avg. loss: \t{test_loss:.4f}, Acc: \t{cur_acc:.1f}%)'
+    P = P.format(epoch=epoch, test_loss=losses.avg, cur_acc=top1.avg)
     print('\r{}'.format(P), end="")
 
-    data.to('cpu')
-    return 100. * correct / nSamples, test_loss
+    return losses.avg, top1.avg
 
 
 def save_checkpoint(state, is_best, epoch, modelDir):
@@ -170,6 +112,10 @@ def save_checkpoint(state, is_best, epoch, modelDir):
     torch.save(state, checkPointPath)
     if is_best:
         shutil.copyfile(checkPointPath, '{}/{}'.format(modelDir, 'best.pth'))
+
+
+def get_num_params(model):
+    return sum([np.prod(p.shape) for p in model.parameters()])
 
 
 def trainAndTest(model, optimizer=None, modelDir=None, epochs=5, targetTranslator=None, model_save_freq=1,
@@ -220,50 +166,43 @@ def trainAndTest(model, optimizer=None, modelDir=None, epochs=5, targetTranslato
     if lr_scheduler is not None:  # bring the lr scheduler up to date.
         lr_scheduler.last_epoch = last_epoch
 
-    for epoch in range(last_epoch, epochs):  # epochs + 1):
+    best_acc = 0
 
+    for epoch in range(last_epoch, epochs):
         if lr_scheduler is not None:
             lr_scheduler.step()
+        train_loss, train_acc = train(model=model, epoch=epoch, optimizer=optimizer,
+                                      train_loader=train_loader, criterion=criterion,  maxIters=maxIters, device=device)
+        all_train_losses.append(train_loss)
+        all_train_accuracies.append(train_acc)
+        test_loss, test_acc = test(model, epoch, test_loader=test_loader,
+                                   criterion=criterion, maxIters=maxIters, device=device)
+        all_val_losses.append(test_loss)
+        all_accuracies.append(test_acc)
 
-        losses, train_acc = train(model=model, epoch=epoch, optimizer=optimizer, targetTranslator=targetTranslator,
-                                  train_loader=train_loader, criterion=criterion,  maxIters=maxIters, device=device, logger=logger)
+        is_best = False
+        if test_acc > best_acc:
+            best_acc = test_acc
+            is_best = True
 
-        cur_acc, cur_test_loss = test(model, epoch, targetTranslator=targetTranslator, test_loader=test_loader,
-                                      prev_acc=best_acc, criterion=criterion, maxIters=maxIters, device=device, logger=logger)
-        all_train_losses.append(losses[-1])
-        all_val_losses.append(cur_test_loss)
-        all_accuracies.append(cur_acc)
-        all_train_accuracies.append(100 * train_acc)
-        corrects.append(cur_acc)
-
-        if needToSave and (epoch % model_save_freq == 0 or epoch == epochs - 1):
-            print('saving model...',)
-            checkPointPath = '{}/{}'.format(modelDir, epoch)
-            if cur_acc > best_acc:
-                best_acc = cur_acc
-                is_best = True
-            else:
-                is_best = False
-            save_checkpoint({
-                'epoch': epoch + 1,
-                'all_train_losses': all_train_losses,
-                'all_val_losses': all_val_losses,
-                'all_accuracies': all_accuracies,
-                'all_train_accuracies': all_train_accuracies,
-                'last_epoch_losses': losses,
-                'state_dict': model.state_dict(),
-                'best_acc': best_acc,
-                'cur_acc': cur_acc,
-                'train_acc': train_acc
-            }, is_best, epoch, modelDir)
-    # return the current state.
-    return {'all_train_losses': all_train_losses,
-            'all_val_losses': all_val_losses,
-            'all_accuracies': all_accuracies,
-            'all_train_accuracies': all_train_accuracies,
-            'last_epoch_losses': losses,
-            'state_dict': model.state_dict(),
-            'best_acc': best_acc,
-            'cur_acc': cur_acc,
-            'train_acc': train_acc
-            }
+        if needToSave:
+            if is_best or (epoch % model_save_freq) == 0 or (epoch == epochs - 1):
+                print('\n\n******saving model******',)
+                save_checkpoint({
+                    'epoch': epoch + 1,
+                    'all_train_losses': all_train_losses,
+                    'all_val_losses': all_val_losses,
+                    'all_accuracies': all_accuracies,
+                    'all_train_accuracies': all_train_accuracies,
+                    'state_dict': model.state_dict(),
+                    'num_params': get_num_params(model),
+                }, is_best, epoch, modelDir)
+    return {
+        'epoch': epoch + 1,
+        'all_train_losses': all_train_losses,
+        'all_val_losses': all_val_losses,
+        'all_accuracies': all_accuracies,
+        'all_train_accuracies': all_train_accuracies,
+        'state_dict': model.state_dict(),
+        'num_params': get_num_params(model),
+    }
